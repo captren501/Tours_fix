c1d537dc292f086daa659bacc05d19b2c2f030bf
ethan langford <89049662+captren501@users.noreply.github.com>
2022-12-15 15:16:59 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Tours from './Pages/Tours';
+
+
+
+function App() {
+  return (
+    <div>
+      <Tours />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/Pages/Tours.jsx
@@ -0,0 +1,15 @@
+
+
+
+const url = 'https://course-api.com/react-tours-project'
+
+const Tours = () => {
+
+    return (
+        <div>
+         
+        </div>
+      )
+};
+
+export default Tours;
