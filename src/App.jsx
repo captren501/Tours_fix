e8937551c008b3a5bf4a232801a3bc5915fa290d
ethan langford <capt.rex64@gmail.com>
2022-12-15 15:44:22 -0700

    i tride
    tring to get the tours link to work

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Tours from './Pages/Tours';
-
+import Data from './data';
 
 
 function App() {
   return (
     <div>
+      <Data />
       <Tours />
     </div>
   )
